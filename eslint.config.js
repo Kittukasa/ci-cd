dbb8b3741629d3945494b0cd70dc01c51c196382
Kittukasa <kittukasa28@gmail.com>
2026-03-09 23:31:26 +0530

    fix eslint jest globals

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,16 @@ import js from '@eslint/js';
 export default [
   js.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        test: 'readonly',
+        expect: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+      },
+    },
     rules: {
       'no-unused-vars': 'warn',
       'no-console': 'warn',
